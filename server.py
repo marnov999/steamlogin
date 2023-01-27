ce77318db02a852c71371ea6388e3a32a14ffe52
marnov999 <87028848+marnov999@users.noreply.github.com>
2023-01-27 21:47:07 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,11 @@
 from flask import Flask, redirect, request
 from json import dumps
 from urllib.parse import urlencode
-
+import requests,json
 app = Flask(__name__)
-app.debug = True
-
+app.debug = False
 steam_openid_url = 'https://steamcommunity.com/openid/login'
-
-@app.route("/")
-def hello():
-    return '<a href="http://localhost:5000/auth">Login with steam</a>'
+json = {}
 
 @app.route("/auth")
 def auth_with_steam():
@@ -19,19 +15,52 @@ def auth_with_steam():
     'openid.identity': "http://specs.openid.net/auth/2.0/identifier_select",
     'openid.claimed_id': "http://specs.openid.net/auth/2.0/identifier_select",
     'openid.mode': 'checkid_setup',
-    'openid.return_to': 'http://127.0.0.1:5000/authorize',
+    'openid.return_to': 'http://127.0.0.1:5000/authorize?season_id='+request.args.get("season_id"),
     'openid.realm': 'http://127.0.0.1:5000'
   }
-
   query_string = urlencode(params)
-  auth_url = steam_openid_url + "?" + query_string
-  print(auth_url)
+  auth_url = steam_openid_url + "?"+ query_string
   return redirect(auth_url)
 
 @app.route("/authorize")
 def authorize():
-  print(request.args)
-  return dumps(request.args) + '<br><br><a href="http://localhost:5000/auth">Login with steam</a>'
+  print(request.args) 
+  print(request.args.get("season_id")) 
+  params = {'key': '2C7FF22BA6152ADD2C1A62E5760A8F75','format': 'json','steamids': request.args.get("openid.identity").split('/id/')[1]}
+  response = requests.get('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/',params=params).json()
+  json[request.args.get("season_id")] = {"name" : response['response']['players'][0]['personaname'],"id":request.args.get("openid.identity").split('/id/')[1]} 
+  return redirect("https://katocraftt.pages.dev/", code=302)
+
+@app.route("/verify")
+def verify():
+  try:
+      print(request.args.get("season_id"))
+      if request.args.get("season_id") in json:
+        return {'status' : True,'name':json[request.args.get("season_id")]['name']}
+      return {'status' : False}
+  except:
+    return {'status' : False}
+    
+@app.route("/add")
+def add():
+  try:
+      print(request.args.get("season_id"))
+      if request.args.get("season_id") in json:
+        return {'status' : True,'name':json[request.args.get("season_id")]['name']}
+      return {'status' : False}
+  except:
+    return {'status' : False}
+@app.route("/remove")
+def remove():
+  try:
+      print(request.args.get("season_id"))
+      if request.args.get("season_id") in json:
+        return {'status' : True,'name':json[request.args.get("season_id")]['name']}
+      return {'status' : False}
+  except:
+    return {'status' : False}
 
 if __name__ == "__main__":
     app.run()
+
+
